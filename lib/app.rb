5ec84609e36b88ade69f4507806d9ba5b68298b6
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-10 16:38:22 +0100

    Add named cat parameter

--- lib/app.rb
+++ lib/app.rb
@@ -12,8 +12,13 @@ get '/another_secret' do
   "boo"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
+end
 
+get '/named-cat' do
+  p params[:name]
+  @name = params[:name]
+  erb(:index)
 end
