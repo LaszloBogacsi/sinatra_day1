e108a40167069cdafc549b51286f489fad254819
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-10 15:26:28 +0100

    Add cat with border

--- lib/app.rb
+++ lib/app.rb
@@ -11,3 +11,14 @@ end
 get '/another_secret' do
   "boo"
 end
+
+get '/cat' do
+  "<HTML>
+  <style>
+  img {
+    border: 3px dashed red;
+      }
+  </style>
+    <img src='http://bit.ly/1eze8aE'>
+  </HTML>"
+end
